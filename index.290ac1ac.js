fd84a75e0d16f80a711f3918afc373c79469905f
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2023-06-03 11:17:56 +0000

    Deploying to gh-pages from @ SGlinkau/project-group-6@73ef630e29bb5eb4b17b47eb68875cd2bc8172e8 🚀

---
+++ index.290ac1ac.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelector("[data-modal-open-sub]"),closeModalBtn:document.querySelector("[data-modal-close-sub]"),modal:document.querySelector("[data-modal-sub]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",o),e.closeModalBtn.addEventListener("click",o)})();
+//# sourceMappingURL=index.290ac1ac.js.map
