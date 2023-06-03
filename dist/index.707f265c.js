828b7008364c25e2de3b038b8b3a75b58a572359
Andrzej Sagan <asagan@ascorp.pl>
2023-06-03 16:32:29 +0200

    update delivery

---
+++ dist/index.707f265c.js
@@ -0,0 +1,33 @@
+(()=>{
+    const mobileMenu = document.querySelector(".js-menu-container");
+    const openMenuBtn = document.querySelector(".js-open-menu");
+    const closeMenuBtn = document.querySelector(".js-close-menu");
+    const closeMenuBtna = document.querySelector(".js-close-menua");
+    const closeMenuBtnb = document.querySelector(".js-close-menub");
+    const closeMenuBtnc = document.querySelector(".js-close-menuc");
+    const closeMenuBtnd = document.querySelector(".js-close-menud");
+    const closeMenuBtne = document.querySelector(".js-close-menue");
+    const toggleMenu = ()=>{
+        const isMenuOpen = openMenuBtn.getAttribute("aria-expanded") === "true" || false;
+        openMenuBtn.setAttribute("aria-expanded", !isMenuOpen);
+        mobileMenu.classList.toggle("is-open");
+        const scrollLockMethod = !isMenuOpen ? "disableBodyScroll" : "enableBodyScroll";
+        bodyScrollLock[scrollLockMethod](document.body);
+    };
+    openMenuBtn.addEventListener("click", toggleMenu);
+    closeMenuBtn.addEventListener("click", toggleMenu);
+    closeMenuBtna.addEventListener("click", toggleMenu);
+    closeMenuBtnb.addEventListener("click", toggleMenu);
+    closeMenuBtnc.addEventListener("click", toggleMenu);
+    closeMenuBtnd.addEventListener("click", toggleMenu);
+    closeMenuBtne.addEventListener("click", toggleMenu);
+    // Close the mobile menu on wider screens if the device orientation changes
+    window.matchMedia("(min-width: 768px)").addEventListener("change", (e)=>{
+        if (!e.matches) return;
+        mobileMenu.classList.remove("is-open");
+        openMenuBtn.setAttribute("aria-expanded", false);
+        bodyScrollLock.enableBodyScroll(document.body);
+    });
+})();
+
+//# sourceMappingURL=index.707f265c.js.map
