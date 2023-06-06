9f3bd93ce29eb1d998154601804bab76c21d9307
SGlinkau <97282786+SGlinkau@users.noreply.github.com>
2023-06-06 17:28:48 +0000

    Deploying to gh-pages from @ SGlinkau/project-group-6@0eb5df2cf2c76ac8f728bc0bfbd1c907b8a5217e 🚀

---
+++ index.22874d20.js
@@ -0,0 +1,2 @@
+(()=>{const e={openModalBtn:document.querySelectorAll("[data-modal-open]"),closeModalBtn:document.querySelectorAll("[data-modal-close]"),modal:document.querySelector("[data-modal]")};function o(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.forEach((e=>{e.addEventListener("click",o)})),e.closeModalBtn.forEach((e=>{e.addEventListener("click",o)}))})();
+//# sourceMappingURL=index.22874d20.js.map
--- index.dc8323f5.js
+++
@@ -1,2 +0,0 @@
-(()=>{const e={openModalBtn:document.querySelector("[data-modal-open-tablet]"),closeModalBtn:document.querySelector("[data-modal-close-tablet]"),modal:document.querySelector("[data-modal]")};function t(){e.modal.classList.toggle("is-hidden")}e.openModalBtn.addEventListener("click",t),e.closeModalBtn.addEventListener("click",t)})();
-//# sourceMappingURL=index.dc8323f5.js.map
